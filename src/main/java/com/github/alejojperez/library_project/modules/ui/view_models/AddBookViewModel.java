2d8fa31bfe3fa88b9e708bfda1635c1ac236ed3c
Alejandro Perez <alejandro@ultimatejetvacations.com>
2016-11-25 12:33:35 -0500

    Added validation to the add book form

--- src/main/java/com/github/alejojperez/library_project/modules/ui/view_models/AddBookViewModel.java
+++ src/main/java/com/github/alejojperez/library_project/modules/ui/view_models/AddBookViewModel.java
@@ -1,13 +1,19 @@
 package com.github.alejojperez.library_project.modules.ui.view_models;
 
+import com.github.alejojperez.library_project.App;
 import com.github.alejojperez.library_project.modules.data.entities.Book;
 import com.github.alejojperez.library_project.modules.data.repositories.BooksRepository;
+import com.github.alejojperez.library_project.modules.ui.views.AddBookView;
 import com.google.inject.Inject;
 import de.saxsys.mvvmfx.ViewModel;
 import de.saxsys.mvvmfx.utils.commands.Action;
 import de.saxsys.mvvmfx.utils.commands.Command;
 import de.saxsys.mvvmfx.utils.commands.DelegateCommand;
 import de.saxsys.mvvmfx.utils.notifications.NotificationCenter;
+import de.saxsys.mvvmfx.utils.validation.CompositeValidator;
+import de.saxsys.mvvmfx.utils.validation.ObservableRuleBasedValidator;
+import de.saxsys.mvvmfx.utils.validation.ValidationMessage;
+import de.saxsys.mvvmfx.utils.validation.ValidationStatus;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -22,6 +28,10 @@ public class AddBookViewModel implements ViewModel
     public StringProperty title = new SimpleStringProperty();
 
     public StringProperty author = new SimpleStringProperty();
+
+    public AddBookViewModel() {
+        this.initializeValidation();
+    }
 
     //<editor-fold desc="AddBookCommand">
 
@@ -42,6 +52,30 @@ public class AddBookViewModel implements ViewModel
         }
 
         return this.addBookCommand;
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="Validation">
+
+    private ObservableRuleBasedValidator titleValidator;
+    public ValidationStatus titleValidation() { return this.titleValidator.getValidationStatus(); }
+
+    private ObservableRuleBasedValidator authorValidator;
+    public ValidationStatus authorValidation() { return this.authorValidator.getValidationStatus(); }
+
+    public CompositeValidator bookFormValidator;
+
+    private void initializeValidation()
+    {
+        this.titleValidator = App.getCurrentDIModule().getInstance(ObservableRuleBasedValidator.class);
+        this.titleValidator.addRule(this.title.isNotEmpty(), ValidationMessage.error("Title field is required."));
+
+        this.authorValidator = App.getCurrentDIModule().getInstance(ObservableRuleBasedValidator.class);
+        this.authorValidator.addRule(this.author.isNotEmpty(), ValidationMessage.error("Author field is required."));
+
+        this.bookFormValidator = App.getCurrentDIModule().getInstance(CompositeValidator.class);
+        this.bookFormValidator.addValidators(this.titleValidator, this.authorValidator);
     }
 
     //</editor-fold>
--- src/main/java/com/github/alejojperez/library_project/modules/ui/views/AddBookView.java
+++ src/main/java/com/github/alejojperez/library_project/modules/ui/views/AddBookView.java
@@ -4,10 +4,17 @@ import com.github.alejojperez.library_project.modules.ui.view_models.AddBookView
 import com.google.inject.Inject;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import de.saxsys.mvvmfx.utils.commands.Action;
 import de.saxsys.mvvmfx.utils.notifications.NotificationCenter;
+import de.saxsys.mvvmfx.utils.validation.ValidationMessage;
+import javafx.collections.ListChangeListener;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,6 +25,12 @@ public class AddBookView implements FxmlView<AddBookViewModel>, Initializable
     public TextField txtTitle;
     @FXML
     public TextField txtAuthor;
+    @FXML
+    public Label lblTitleValidationMessage;
+    @FXML
+    public Label lblAuthorValidationMessage;
+    @FXML
+    public Button btnAddBook;
 
     @InjectViewModel
     private AddBookViewModel viewModel;
@@ -31,13 +44,46 @@ public class AddBookView implements FxmlView<AddBookViewModel>, Initializable
         this.viewModel.author.bindBidirectional(this.txtAuthor.textProperty());
 
         this.notificationCenter.subscribe("module:ui:AddBook", (key, payload) -> {
-            this.txtTitle.setText("");
-            this.txtAuthor.setText("");
+
         });
+
+        /**
+         * Configure title textfield validation
+         */
+        this.viewModel.titleValidation().getMessages().addListener((ListChangeListener<ValidationMessage>) c -> {
+            String text = null;
+
+            if(this.viewModel.titleValidation().getHighestMessage().isPresent())
+                text = this.viewModel.titleValidation().getHighestMessage().get().getMessage();
+
+            this.lblTitleValidationMessage.setText(text);
+        });
+        this.lblTitleValidationMessage.visibleProperty().bind(this.lblTitleValidationMessage.textProperty().isNotEmpty());
+        this.txtTitle.disableProperty().bind(this.viewModel.getAddBookCommand().runningProperty());
+
+        /**
+         * Configure author textfield validation
+         */
+        this.viewModel.authorValidation().getMessages().addListener((ListChangeListener<ValidationMessage>) c -> {
+            String text = null;
+
+            if(this.viewModel.authorValidation().getHighestMessage().isPresent())
+                text = this.viewModel.authorValidation().getHighestMessage().get().getMessage();
+
+            this.lblAuthorValidationMessage.setText(text);
+        });
+        this.lblAuthorValidationMessage.visibleProperty().bind(this.lblAuthorValidationMessage.textProperty().isNotEmpty());
+        this.txtAuthor.disableProperty().bind(this.viewModel.getAddBookCommand().runningProperty());
+
+        /**
+         * Configure add book button validation
+         */
+        this.btnAddBook.disableProperty().bind(this.viewModel.bookFormValidator.getValidationStatus().validProperty().not());
     }
 
-    public void saveBook()
+    public void saveBook(ActionEvent event)
     {
         this.viewModel.getAddBookCommand().execute();
+        this.btnAddBook.getScene().getWindow().hide();
     }
 }
